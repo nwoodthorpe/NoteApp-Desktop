166741fc18037f375472bba9d1c35353a9b25199
Nathaniel <Nathaniel@192.168.56.1>
2015-01-27 23:50:32 -0500

    Method added to validate file extensions
    Various small changes
    
    Commenting

--- src/NoteSystem/NoteSystem.java
+++ src/NoteSystem/NoteSystem.java
@@ -25,6 +25,7 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
@@ -37,12 +38,19 @@ public class NoteSystem extends JFrame {
     public String[] columnNames = {"Date",
         "Title",
         "Description",
-        "Format"};
+        "Format"
+    };
 
     Object[][] data = {
         {"", "",
             "", ""}
     };
+
+    public String[] allowedExtensions
+            = {"png", "bmp", "jpeg", "doc", "docx", "jpg"};
+
+    DefaultTableModel dtm;
+    JTable selectionTable;
 
     protected void initUI() {
         JFrame frame = new JFrame();
@@ -50,6 +58,14 @@ public class NoteSystem extends JFrame {
         contentPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //TitlePanel
+        JPanel titlePanel = new JPanel();
+        titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.X_AXIS));
+
+        JLabel title = new JLabel("Title will be Here");
+        title.setFont(new Font("Arial", Font.BOLD, 25));
+        titlePanel.add(title);
 
         //topButtonBar
         JPanel topButtonBar = new JPanel();
@@ -184,8 +200,8 @@ public class NoteSystem extends JFrame {
         tablePanel.setLayout(new BoxLayout(tablePanel, BoxLayout.X_AXIS));
 
         //Set up table and tablemodel
-        DefaultTableModel dtm = new DefaultTableModel(data, columnNames);
-        JTable selectionTable = new JTable(dtm) {
+        dtm = new DefaultTableModel(data, columnNames);
+        selectionTable = new JTable(dtm) {
             @Override
             public boolean isCellEditable(int row, int col) {
                 return false;
@@ -198,6 +214,8 @@ public class NoteSystem extends JFrame {
         tablePanel.add(tableHolder);
 
         //Add everything to the contentPanel
+        contentPanel.add(titlePanel);
+        contentPanel.add(Box.createRigidArea(new Dimension(15, 15)));
         contentPanel.add(topButtonBar);
         contentPanel.add(spacerPanel);
         contentPanel.add(tablePanel);
@@ -207,10 +225,26 @@ public class NoteSystem extends JFrame {
         frame.setVisible(true);
     }
 
+    public boolean extensionAllowed(File file) {
+        String extension = "";
+        int i = file.getPath().lastIndexOf('.');
+        if (i > 0) {
+            extension = file.getPath().substring(i + 1);
+        }
+        
+        extension = extension.toLowerCase();
+        for(int j = 0; j<allowedExtensions.length; j++){
+            if(extension.equals(allowedExtensions[j]))
+                return true;
+        }
+        return false;
+    }
+
     public void buttonPressed() {
         //Create the "Add Note" Dialog
-        JDialog dialog = new JDialog();
-        dialog.setSize(400, 500);
+        final JDialog dialog = new JDialog();
+        dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        dialog.setSize(400, 300);
 
         JPanel contentPanel = (JPanel) dialog.getContentPane();
         contentPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
@@ -234,20 +268,21 @@ public class NoteSystem extends JFrame {
         JPanel selectFilePanel = new JPanel();
         selectFilePanel.setLayout(new BoxLayout(selectFilePanel, BoxLayout.X_AXIS));
 
-        final JLabel selectedFile = new JLabel("");
+        String filename = File.separator + "tmp";
+        final JFileChooser fileChooser = new JFileChooser(new File(filename));
         
-        JButton selectButton = new JButton("Select a File:");
+        final JLabel selectedFile = new JLabel("No File Selected");
+
+        JButton selectButton = new JButton("Select File(s):");
         selectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                String filename = File.separator + "tmp";
-                JFileChooser fileChooser = new JFileChooser(new File(filename));
                 fileChooser.setMultiSelectionEnabled(true);
                 fileChooser.showOpenDialog(null);
-                if(fileChooser.getSelectedFiles().length > 1){
-                    selectedFile.setText("<html>" + fileChooser.getSelectedFile().getName() + "<br>" + 
-                     "and " + (fileChooser.getSelectedFiles().length - 1) + " more.</html>");
-                }else{
+                if (fileChooser.getSelectedFiles().length > 1) {
+                    selectedFile.setText("<html>" + fileChooser.getSelectedFile().getName() + "<br>"
+                            + "and " + (fileChooser.getSelectedFiles().length - 1) + " more.</html>");
+                } else if (!(fileChooser.getSelectedFiles().length == 0)) {
                     selectedFile.setText(fileChooser.getSelectedFile().getName());
                 }
             }
@@ -256,9 +291,9 @@ public class NoteSystem extends JFrame {
         selectFilePanel.add(Box.createRigidArea(new Dimension(20, 2)));
         selectFilePanel.add(selectedFile);
         selectFilePanel.add(Box.createHorizontalGlue());
-        
+
         //Panel for the title of the note
-        JPanel noteTitlePanel = new JPanel(){
+        JPanel noteTitlePanel = new JPanel() {
             @Override
             public Dimension getMaximumSize() {
                 Dimension max = super.getMaximumSize();
@@ -267,17 +302,17 @@ public class NoteSystem extends JFrame {
             }
         };
         noteTitlePanel.setLayout(new BoxLayout(noteTitlePanel, BoxLayout.X_AXIS));
-        
+
         JLabel noteTitleLabel = new JLabel("Title:  ");
         noteTitlePanel.add(noteTitleLabel);
-        
+
         noteTitlePanel.add(Box.createRigidArea(new Dimension(90, 2)));
-        
+
         JTextField noteTitleField = new JTextField("");
         noteTitlePanel.add(noteTitleField);
-        
+
         //Panel for the description of the note
-        JPanel noteDescriptionPanel = new JPanel(){
+        JPanel noteDescriptionPanel = new JPanel() {
             @Override
             public Dimension getMaximumSize() {
                 Dimension max = super.getMaximumSize();
@@ -286,17 +321,17 @@ public class NoteSystem extends JFrame {
             }
         };
         noteDescriptionPanel.setLayout(new BoxLayout(noteDescriptionPanel, BoxLayout.X_AXIS));
-        
+
         JLabel noteDescriptionLabel = new JLabel("Description:  ");
         noteDescriptionPanel.add(noteDescriptionLabel);
-        
+
         noteDescriptionPanel.add(Box.createRigidArea(new Dimension(50, 2)));
-        
+
         JTextField noteDescriptionField = new JTextField("");
         noteDescriptionPanel.add(noteDescriptionField);
-        
+
         //Panel for the tags of the note
-        JPanel noteTagsPanel = new JPanel(){
+        JPanel noteTagsPanel = new JPanel() {
             @Override
             public Dimension getMaximumSize() {
                 Dimension max = super.getMaximumSize();
@@ -305,15 +340,15 @@ public class NoteSystem extends JFrame {
             }
         };
         noteTagsPanel.setLayout(new BoxLayout(noteTagsPanel, BoxLayout.X_AXIS));
-        
+
         JLabel noteTagsLabel = new JLabel("Tags:  ");
         noteTagsPanel.add(noteTagsLabel);
-        
+
         noteTagsPanel.add(Box.createRigidArea(new Dimension(87, 2)));
-        
+
         JTextField noteTagsField = new JTextField("");
         noteTagsPanel.add(noteTagsField);
-        
+
         //Panel for the text below the tags label
         JPanel tagsPanel2 = new JPanel();
         tagsPanel2.setLayout(new BoxLayout(tagsPanel2, BoxLayout.X_AXIS));
@@ -321,7 +356,32 @@ public class NoteSystem extends JFrame {
         JLabel tagsLabel2 = new JLabel("(Seperate with Spaces)");
         tagsPanel2.add(tagsLabel2);
         tagsPanel2.add(Box.createHorizontalGlue());
-        
+
+        //Panel for the 2 buttons
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+
+        JButton addButton = new JButton("Add");
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                System.out.println(extensionAllowed(fileChooser.getSelectedFiles()[0]));
+            }
+        });
+        buttonPanel.add(addButton);
+
+        buttonPanel.add(Box.createRigidArea(new Dimension(30, 30)));
+
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                System.out.println("Cancel Pressed.");
+                dialog.dispose();
+            }
+        });
+        buttonPanel.add(cancelButton);
+
         //Add everything to our contentPanel (Master Panel)
         contentPanel.add(titlePanel);
         contentPanel.add(spacerPanel);
@@ -333,9 +393,10 @@ public class NoteSystem extends JFrame {
         contentPanel.add(Box.createRigidArea(new Dimension(15, 15)));
         contentPanel.add(noteTagsPanel);
         contentPanel.add(tagsPanel2);
+        contentPanel.add(Box.createRigidArea(new Dimension(15, 15)));
+        contentPanel.add(buttonPanel);
         dialog.setModal(true);
         dialog.setVisible(true);
-        System.out.println("dialog closed. tf=");
     }
 
     public static void main(String[] args) {
@@ -344,7 +405,6 @@ public class NoteSystem extends JFrame {
             public void run() {
                 NoteSystem MainWindow = new NoteSystem();
                 MainWindow.initUI();
-
             }
         });
     }
