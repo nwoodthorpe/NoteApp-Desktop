9dd8e04af9c79557af9aab42c40d49cefaf003d8
NATHAN4 <NJWoodthorpe@gmail.com>
2015-02-26 09:16:46 -0500

    More options avaliable when opening notes

--- src/NoteSystem/Form_ListNoteContents.java
+++ src/NoteSystem/Form_ListNoteContents.java
@@ -2,11 +2,19 @@ package NoteSystem;
 
 import java.awt.BorderLayout;
 import java.awt.Desktop;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -46,9 +54,26 @@ public class Form_ListNoteContents extends JDialog {
         return files;
     }
 
+    private void openFileViewer(File file) {
+        try {
+            
+            Desktop.getDesktop().open(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void openFileExplorer(File file){
+        try {
+            Runtime.getRuntime().exec("explorer.exe /select,"+file.getPath());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     public void genGUI() {
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        setSize(300, 400);
+        setSize(400, 400);
 
         JPanel contentPanel = (JPanel) getContentPane();
         contentPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
@@ -71,27 +96,65 @@ public class Form_ListNoteContents extends JDialog {
             strippedNames[i] = noteFiles[i].getName();
         }
 
-        JList list = new JList(strippedNames);
+        final JButton explorerButton = new JButton("Explorer");
+        final JButton viewerButton = new JButton("Default Viewer");
+
+        final JList list = new JList(strippedNames);
         list.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent evt) {
                 JList list = (JList) evt.getSource();
                 if (evt.getClickCount() == 2) {
-                    try {
-                        File file = noteFiles[list.getSelectedIndex()];
-                        Desktop.getDesktop().open(file);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                    openFileViewer(noteFiles[list.getSelectedIndex()]);
+                }
+                if (list.getSelectedIndex() != -1) {
+                    explorerButton.setEnabled(true);
+                    viewerButton.setEnabled(true);
+                } else {
+                    explorerButton.setEnabled(false);
+                    viewerButton.setEnabled(false);
                 }
 
             }
         });
         scrollPanel.add(list, BorderLayout.CENTER);
 
-        contentPanel.add(titlePanel);
+        //Panel for the buttons
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
 
+        JLabel openLabel = new JLabel("Open using:");
+
+        explorerButton.setEnabled(false);
+        explorerButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                openFileExplorer(noteFiles[list.getSelectedIndex()]);
+            }
+
+        });
+
+        viewerButton.setEnabled(false);
+        viewerButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                openFileViewer(noteFiles[list.getSelectedIndex()]);
+            }
+
+        });
+
+        buttonPanel.add(openLabel);
+        buttonPanel.add(Box.createHorizontalGlue());
+        buttonPanel.add(explorerButton);
+        buttonPanel.add(Box.createRigidArea(new Dimension(15, 15)));
+        buttonPanel.add(viewerButton);
+
+        contentPanel.add(titlePanel);
         contentPanel.add(scrollPanel);
+        contentPanel.add(Box.createRigidArea(new Dimension(15, 15)));
+        contentPanel.add(buttonPanel);
 
         setModal(true);
         setVisible(true);
